e2c459134c10c05da6cec676d16af172086a7a09
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-04-15 19:02:04 +0300

    add hr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
     <main className="max-w-xl mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Welcome in ToDo App</h1>
       <AddTodo />
+      <div className="flex justify-center">
+        <hr className="w-50 text-gray-200 mt-4 mb-6" />
+      </div>
       <TodoList />
     </main>
   );
